c7147c88cf06cf74ce3875729f8e621f7858709e
C-gyeltshen <02230279.cst@rub.edu.bt>
2025-10-16 20:57:07 +0600

    hjsbdjkf

--- services/user_services/database/db.go
+++ services/user_services/database/db.go
@@ -2,10 +2,12 @@ package database
 
 import (
 	"log"
-	"os" 
+	"os"
+	"time"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"user_services/models" 
+	"gorm.io/gorm/logger"
+	"user_services/models"
 )
 
 var DB *gorm.DB
@@ -23,14 +25,44 @@ func Connect() error {
 
     log.Println("Attempting to connect to database using DSN from environment...")
 
-    // 2. Open the database connection
-    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    // 2. Configure GORM logger
+    // Set log level to Warn to reduce noise from slow migration queries
+    // You can change to logger.Info if you want to see all queries during development
+    gormLogger := logger.Default.LogMode(logger.Warn)
+
+    // Optionally adjust the slow query threshold (default is 200ms)
+    gormLogger = logger.New(
+        log.New(os.Stdout, "\r\n", log.LstdFlags),
+        logger.Config{
+            SlowThreshold:             time.Second,   // Queries slower than 1 second are logged
+            LogLevel:                  logger.Warn,   // Log level: Silent, Error, Warn, Info
+            IgnoreRecordNotFoundError: true,          // Don't log ErrRecordNotFound errors
+            Colorful:                  false,         // Disable color in Docker logs
+        },
+    )
+
+    // 3. Open the database connection
+    DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+        Logger: gormLogger,
+    })
     if err != nil {
         log.Printf("Error connecting to database: %v", err)
         return err
     }
 
-    // 3. AutoMigrate the models
+    // 4. Configure connection pooling for better performance
+    sqlDB, err := DB.DB()
+    if err != nil {
+        log.Printf("Error getting database instance: %v", err)
+        return err
+    }
+
+    // Set connection pool settings
+    sqlDB.SetMaxIdleConns(10)           // Maximum number of idle connections
+    sqlDB.SetMaxOpenConns(100)          // Maximum number of open connections
+    sqlDB.SetConnMaxLifetime(time.Hour) // Maximum lifetime of a connection
+
+    // 5. AutoMigrate the models
     // GORM will create or update the tables based on your structs
     err = DB.AutoMigrate(&models.UserData{}, &models.UserRole{})
     if err != nil {
